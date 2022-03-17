2feb662fbe499e5981d3f50571d6d5102df91375
Anton Radchenko <anthony.by.ateo@gmail.com>
2022-03-17 17:50:23 +0300

    test

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import './index.css';
 // + Add a toggle button that lets you sort the moves in either ascending or descending order.
 // - When someone wins, highlight the three squares that caused the win.
 // - When no one wins, display a message about the result being a draw
+// test
 
 function Square(props) {
   return (
